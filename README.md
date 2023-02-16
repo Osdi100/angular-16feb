e839c6aea4b2c541ad811effe62ba1e2813e54e0
Osdi100 <106705591+Osdi100@users.noreply.github.com>
2023-02-16 14:47:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-16feb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-jwhmax)
---
+++ src/app/products.ts
@@ -0,0 +1,44 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  stock: number;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    stock: 10,
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    stock: 0,
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    stock: 2,
+  },
+  {
+    id: 4,
+    name: 'Poco Phone',
+    price: 500,
+    description: 'Poco x3 pro',
+    stock: 11,
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
